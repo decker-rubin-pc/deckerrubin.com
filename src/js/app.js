3b21c081085bf45e48f1e063c6f4a76e257ba806
Michael Lawrence <me@mikelawrence.co>
2019-08-17 15:03:04 -0400

    Beginning to work on making links become active when the corresponding page is visible.

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,35 @@
       .then(function () { console.log('Service Worker Registered'); });
   }
 
+  const $anchors = [...document.querySelectorAll('a')];
+  const $validAnchors = $anchors.filter($el => $el.getAttribute('href'));
+  const $externalAnchors = $validAnchors.filter($el => $el.hostname !== location.hostname);
+  const $navAnchors = $validAnchors
+    .filter($el => $el.getAttribute('href') !== '#')
+    .filter($el => $el.getAttribute('href').startsWith('#'));
+
   // Open external links in new window
-  [...document.getElementsByTagName('a')]
-    .filter($el => $el.getAttribute('href'))
-    .filter($el => $el.hostname !== location.hostname)
-    .forEach($el => $el.target = '_blank');
+  $externalAnchors.forEach($el => $el.target = '_blank');
+
+  // Navigation
+  // @todo Tighten this up. Contact page can never be active.
+  const $sections = [...document.querySelectorAll('section')];
+
+  const intersectionObserver = new IntersectionObserver(entries => {
+    const firstVisible = entries.find(entry => entry.intersectionRatio > 0);
+
+    if (firstVisible) {
+      const id = firstVisible.target.id;
+      const $activeNavAnchors = $navAnchors.filter($el => $el.getAttribute('href') === `#${id}`);
+
+      if ($activeNavAnchors.length) {
+        $navAnchors.forEach($el => $el.classList.remove('active'));
+        $activeNavAnchors.forEach($el => $el.classList.add('active'));
+      }
+    }
+  });
+
+  $sections.forEach($el => intersectionObserver.observe($el));
 
   // Podcast
 
