57a3d5f2a9cd07dc91191f02bdb1640a874d8972
Michael Lawrence <me@mikelawrence.co>
2019-08-17 13:45:59 -0400

    Contact Page styles.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
 (async () => {
+  // Register service worker
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('/js/sw.js')
       .then(function () { console.log('Service Worker Registered'); });
   }
+
+  // Open external links in new window
+  [...document.getElementsByTagName('a')]
+    .filter($el => $el.getAttribute('href'))
+    .filter($el => $el.hostname !== location.hostname)
+    .forEach($el => $el.target = '_blank');
+
+
+  // Podcast
 
   //const podcastRSS = '/podcast/rss';
   //const response = await fetch(podcastRSS);
